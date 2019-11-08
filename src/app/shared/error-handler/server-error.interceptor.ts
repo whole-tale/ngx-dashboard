52bc703d610b26fd560d0d6042e77d55cbbd6293
Mike Lambert <lambert8@illinois.edu>
2019-11-08 14:52:32 -0600

    fix: route to login if 401 is encountered

--- src/app/shared/error-handler/server-error.interceptor.ts
+++ src/app/shared/error-handler/server-error.interceptor.ts
@@ -1,20 +1,24 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { LogService } from '@framework/core/log.service';
+import { AuthService } from '@ngx-auth/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
 @Injectable()
 export class ServerErrorInterceptor implements HttpInterceptor {
-  constructor(private readonly logger: LogService) {}
+  constructor(private readonly logger: LogService, private readonly router: Router, private readonly auth: AuthService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       retry(1),
       catchError((error: HttpErrorResponse) => {
         if (error.status === 401) {
-          // refresh token
-          this.logger.debug('//refresh token plz');
+          // refresh token plz
+          this.auth.invalidate().then(resp => {
+            this.router.navigate(['login']);
+          });
         } else {
           return throwError(error);
         }
