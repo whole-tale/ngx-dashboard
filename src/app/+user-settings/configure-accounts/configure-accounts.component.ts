90a792c6b6d41d7cd6a200d2c76e9d82ebe8bb61
Mike Lambert <lambert8@illinois.edu>
2020-08-17 13:30:16 -0500

    fix: improve error handling in configure-accounts view

--- src/app/+user-settings/configure-accounts/configure-accounts.component.ts
+++ src/app/+user-settings/configure-accounts/configure-accounts.component.ts
@@ -8,6 +8,7 @@ import { AccountService } from '@api/services/account.service';
 import { UserService } from '@api/services/user.service';
 import { BaseComponent } from '@framework/core/base.component';
 import { LogService } from '@framework/core/log.service';
+import { ErrorService } from '@shared/error-handler/services/error.service';
 
 import { ConnectApiKeyModalComponent } from '../modals/connect-apikey-modal/connect-apikey-modal.component';
 import { DisconnectAccountModalComponent } from '../modals/disconnect-account-modal/disconnect-account-modal.component';
@@ -39,7 +40,7 @@ interface UserToken {
 export class ConfigureAccountsComponent extends BaseComponent implements OnInit {
   tokens: Array<UserToken> = [];
   providers: Array<Account> = [];
-  
+
   constructor(
     private readonly ref: ChangeDetectorRef,
     private zone: NgZone,
@@ -47,11 +48,12 @@ export class ConfigureAccountsComponent extends BaseComponent implements OnInit 
     private readonly userService: UserService,
     private readonly logger: LogService,
     private http: HttpClient,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private errorHandler: ErrorService
   ) {
       super();
   }
-  
+
   ngOnInit(): void {
     this.refreshProviders();
   }
@@ -63,37 +65,37 @@ export class ConfigureAccountsComponent extends BaseComponent implements OnInit 
   trackByProviderName(index: number, token: any): string {
     if (!token.provider) {
       return token;
-    } 
-    
+    }
+
     return token.provider.name;
   }
-  
+
   refreshTokens(): void {
     this.userService.userGetMe().subscribe((user: User) => {
       if (!user || !user.otherTokens) {
         this.logger.error("Failed to fetch user:", user);
-        
+
         return;
       }
-      
+
       const tokens = user.otherTokens;
       this.tokens = tokens;
-      
+
       this.logger.info('Fetched user tokens:', tokens);
-      
+
       this.providers.forEach(provider => {
         if (provider.type === 'dataone' && provider.state === 'preauthorized') {
           this.logger.info("Fetching DataONE JWT");
           this.fetchDataOneJwt(user, provider);
         }
       });
-      
+
       this.ref.detectChanges();
     }, (err: any) => {
-      console.error('Failed to fetch user:', err);
+      this.logger.error('Failed to fetch user:', err);
     });
   }
-  
+
   refreshProviders(): void {
     const redirect = `${window.origin}/settings`;
     const params = { redirect };
@@ -102,26 +104,26 @@ export class ConfigureAccountsComponent extends BaseComponent implements OnInit 
       this.refreshTokens();
       this.logger.info('Fetched external accounts:', accts);
     }, (err: any) => {
-      console.error('Failed to fetch accounts:', err);
+      this.logger.error('Failed to fetch accounts:', err);
     });
   }
-  
+
   rerouteToOAuthLogin(provider: Account): boolean {
     if (provider.type === 'apikey') {
       return false;
     }
-    
+
     // TODO: For DataONE/OAuth, just redirect to their login
     window.location.href = provider.url;
-    
+
     return true;
   }
-  
+
   fetchDataOneJwt(user: User, provider: Account): void {
     if (provider.type !== 'dataone') {
       // Only fetch JWT for DataONE provider(s)
       this.logger.error("Cannot fetch JWT for non-DataONE account providers.");
-      
+
       return;
     }
 
@@ -132,7 +134,7 @@ export class ConfigureAccountsComponent extends BaseComponent implements OnInit 
         'Content-Type':  'text/xml',
       })
     };
-    
+
     this.http.get(provider.url, httpOptions).subscribe((resp: string) => {
       // If we get a response, POST it back to Girder as an API key
       const jwt = resp;
@@ -140,35 +142,35 @@ export class ConfigureAccountsComponent extends BaseComponent implements OnInit 
       const token = user.otherTokens.find((t: UserToken) => t.provider && t.provider === provider.name);
       this.connectProvider(provider.name, token.resource_server, jwt, KeyType.DataONE);
     }, (err: any) => {
-      console.error("Failed to fetch DataONE JWT:", err);
+      this.logger.error("Failed to fetch DataONE JWT:", err);
     });
   }
-  
+
   openConnectApiKeyModal(provider: Account, event: Event): void {
-    const dialogRef = this.dialog.open(ConnectApiKeyModalComponent);
+    const dialogRef = this.dialog.open(ConnectApiKeyModalComponent, { data: { provider }});
     dialogRef.afterClosed().subscribe((selectedResult: UserToken) => {
       if (!selectedResult) { return; }
-      
+
       // TODO: connect APIKey account
       this.connectProvider(selectedResult.provider, selectedResult.resource_server, selectedResult.access_token, selectedResult.token_type);
     });
   }
-  
+
   openDisconnectAccountModal(provider: Account, token: UserToken, event: Event): void {
-    const dialogRef = this.dialog.open(DisconnectAccountModalComponent);
+    const dialogRef = this.dialog.open(DisconnectAccountModalComponent, { data: { provider }});
     dialogRef.afterClosed().subscribe((selectedResult: any) => {
       if (!selectedResult) { return; }
-      
+
       // TODO: disconnect account
       this.disconnectProvider(provider.name, token.resource_server);
     });
   }
-  
+
   clearConnectExtAcctModal(): void {
     // this.newApiKey = '';
     // this.newResourceServer = '';
   }
-  
+
   connectProvider(providerName: string, resourceServer: string, apiKey: string, keyType: KeyType = KeyType.ApiKey): void {
     const params = {
       'provider': providerName,
@@ -176,28 +178,29 @@ export class ConfigureAccountsComponent extends BaseComponent implements OnInit 
       'key': apiKey,
       'key_type': keyType
     };
-    
+
     this.accountService.accountCreateApiKeyAccount(params).subscribe(resp => {
       this.logger.info("Successfully attached API key:", resp);
       this.refreshProviders();
     }, err => {
-      console.error("Failed to attach API key:", err);
+      this.logger.error("Failed to attach API key:", err);
+      this.errorHandler.showErrorModal("Failed to attach API key", err);
     });
-    
+
     this.clearConnectExtAcctModal();
   }
-  
+
   disconnectProvider(providerName: string, resourceServer: string): void {
     const params = {
       'provider': providerName,
       'resource_server': resourceServer,
     };
-    
+
     this.accountService.accountRevokeAccount(params).subscribe(resp => {
       this.logger.info("Successfully revoked API key:", resp);
       this.refreshProviders();
     }, err => {
-      console.error("Failed to revoke API key:", err);
+      this.logger.error("Failed to revoke API key:", err);
     });
   }
 }
