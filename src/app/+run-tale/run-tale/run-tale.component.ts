d51a044c81172fce7f732100b6dc2cc14f1bea7f
Mike Lambert <lambert8@illinois.edu>
2021-01-21 15:46:29 -0600

    fix: redirect from run-tale to catalog on error

--- src/app/+run-tale/run-tale/run-tale.component.ts
+++ src/app/+run-tale/run-tale/run-tale.component.ts
@@ -145,7 +145,7 @@ export class RunTaleComponent extends BaseComponent implements OnInit, OnChanges
                       .subscribe((tale: Tale) => {
         if (!tale) {
           this.logger.error("Tale is null, something went horribly wrong");
-
+          this.router.navigate(['public']);
           return;
         }
 
@@ -170,6 +170,7 @@ export class RunTaleComponent extends BaseComponent implements OnInit, OnChanges
         });
       }, err => {
         this.logger.error("Failed to fetch tale:", err);
+        this.router.navigate(['public']);
       });
     }
 
