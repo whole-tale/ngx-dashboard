0ff694af0372d216fdad63b039d6bcf047247866
Mike Lambert <lambert8@illinois.edu>
2021-07-14 13:02:56 -0500

    fix: add error handling for env.json fetch

--- src/app/shared/core/window.service.ts
+++ src/app/shared/core/window.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LogService } from '@shared/core/log.service';
 
 import { Window } from './models/window';
 
@@ -11,12 +12,15 @@ export class WindowService implements Window {
   location: any = {};
   env: any = {};
 
-  constructor(private readonly http: HttpClient) {
-    this.http.get(ENV_URL).subscribe(env => {
-      this.env = env;
-    }, err => {
-      this.logger.error("Failed to fetch env: ", err);
-    });
+  constructor(private readonly http: HttpClient, private readonly logger: LogService) {
+    this.http.get(ENV_URL).subscribe(
+      env => {
+        this.env = env;
+      },
+      err => {
+        this.logger.error('Failed to fetch env: ', err);
+      }
+    );
   }
 
   open(url: string, target?: string): void {
