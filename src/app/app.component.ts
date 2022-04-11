916ab01a9c08a2f17bb03eeb298b921d0e8d7a7a
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2022-04-11 11:40:23 -0500

    fix: add accessibility features to the login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { BaseComponent } from '@shared/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
   // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent extends BaseComponent {}
+export class AppComponent extends BaseComponent {
+  constructor(private readonly titleService: Title, private readonly metaService: Meta) {
+    super();
+    this.setTitle('WholeTale Dashboard');
+    this.metaService.updateTag({
+      name: 'description',
+      content: 'Changing meta tags using an Angular service',
+    });
+  }
+  setTitle(newTitle: string): void {
+    this.titleService.setTitle(newTitle);
+  }
+}
