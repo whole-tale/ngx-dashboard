745b45f030339e6dbea7f0d6acd67c75a4903af0
Sara Lambert <lambert8@illinois.edu>
2022-12-19 12:09:05 -0600

    feat: better state detection for Job/Instance log messages

--- src/app/shared/common/components/view-logs-dialog/view-logs-dialog.component.ts
+++ src/app/shared/common/components/view-logs-dialog/view-logs-dialog.component.ts
@@ -20,6 +20,7 @@ export class ViewLogsDialogComponent implements OnInit, OnDestroy {
   jobs: Array<Job> = [];
   intervals: Array<any> = [];
   stayAtBottom = true;
+  fetching = true;
 
   constructor(
     private readonly ref: ChangeDetectorRef,
@@ -92,7 +93,13 @@ export class ViewLogsDialogComponent implements OnInit, OnDestroy {
       logs += `${job.log.join('')}\n\n\n\n\n`;
     });
 
-    this.logs.next(logs);
+    const logsFound = !!logs.replace(/\w/g, '').trim();
+    if (logsFound) {
+      this.logs.next(logs);
+    } else {
+      this.logs.next(`No log data found for jobs with ID = ${this.jobs.map((j) => j._id)}`);
+    }
+
     this.ref.detectChanges();
   }
 
@@ -104,15 +111,28 @@ export class ViewLogsDialogComponent implements OnInit, OnDestroy {
   }
   autoFetchInstanceLogs(intervalMs: number = this.refreshInterval): void {
     const interval: any = setInterval(() => {
-      this.instanceService.instanceGetInstanceLogs(this.data.instance._id).subscribe((logs: string) => {
-        this.logs.next(logs);
-        this.ref.detectChanges();
+      this.instanceService.instanceGetInstanceLogs(this.data.instance._id).subscribe(
+        (logs: string) => {
+          const logsFound = !!logs.replace(/\w/g, '').trim();
+          if (logsFound) {
+            // Print log messages, if any are found
+            this.logs.next(logs);
+          } else {
+            // Print error message if instance logs are empty
+            this.logs.next(`No log data found for instance with ID = ${this.data.instance._id}`);
+          }
+          this.ref.detectChanges();
 
-        // If requested, keep scrolling at the bottom
-        if (this.shouldStayAtBottom()) {
-          this.scrollToBottom();
+          // If requested, keep scrolling at the bottom
+          if (this.shouldStayAtBottom()) {
+            this.scrollToBottom();
+          }
+        },
+        (err) => {
+          // Print error message if we failed to fetch instance logs
+          this.logs.next(`Failed to fetch logs for instance with ID = ${this.data.instance._id}:\n${err?.message || err?.error || err}`);
         }
-      });
+      );
     }, intervalMs);
 
     this.intervals.push(interval);
@@ -120,25 +140,32 @@ export class ViewLogsDialogComponent implements OnInit, OnDestroy {
 
   autoFetchJobLogs(jobId: string, intervalMs: number = this.refreshInterval): void {
     const interval: any = setInterval(() => {
-      this.jobService.jobGetJob(jobId).subscribe((job: Job) => {
-        // If job is done, stop polling
-        if (job.status === 3) {
-          this.stopPolling(interval);
+      this.fetching = true;
+      this.jobService.jobGetJob(jobId).subscribe(
+        (job: Job) => {
+          // If job is done, stop polling
+          if (job.status === 3) {
+            this.stopPolling(interval);
+          }
+
+          // Overwrite existing log data for this job
+          const existing = this.jobs.find((j) => jobId === j._id);
+          const index = this.jobs.indexOf(existing);
+          this.jobs[index] = job;
+          this.fetching = false;
+
+          // Concatenate all logs together for display
+          this.mergeLogs();
+
+          // If requested, keep scrolling at the bottom
+          if (this.shouldStayAtBottom()) {
+            this.scrollToBottom();
+          }
+        },
+        (err) => {
+          this.logs.next(`No log data found for jobs with ID = ${this.jobs.map((j) => j._id)}:\n${err?.message || err?.error || err}`);
         }
-
-        // Overwrite existing log data for this job
-        const existing = this.jobs.find((j) => jobId === j._id);
-        const index = this.jobs.indexOf(existing);
-        this.jobs[index] = job;
-
-        // Concatenate all logs together for display
-        this.mergeLogs();
-
-        // If requested, keep scrolling at the bottom
-        if (this.shouldStayAtBottom()) {
-          this.scrollToBottom();
-        }
-      });
+      );
     }, intervalMs);
 
     this.intervals.push(interval);
